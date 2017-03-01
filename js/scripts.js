26279fa74ed3e42b95141ebd1e13b1c92a9aa20b
Łukasz Wasiluk <galawanty@gmail.com>
2017-03-01 22:25:35 +0100

    plik html i js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// bok a = 5cm
+// wysokość = 10cm
+// pole = 25
+var a = 5
+var h = 10
+var triangleArea = a*h/2
+document.write(triangleArea)
+console.log('Pole trójkąta o podstawie a: ' + 5 + ' i wysokości h: ' + 10 + ' wynosi: ' + triangleArea);
